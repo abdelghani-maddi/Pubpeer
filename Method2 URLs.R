476cb1a2ec46c21017c44c4ea33a970c2c6f8cc2
Abdelghani Maddi <maddiabdelghani@gmail.com>
2023-03-08 08:46:34 +0100

    commit 08-03-23

--- Method2 URLs.R
+++ Method2 URLs.R
@@ -28,6 +28,8 @@ library(gtsummary)
 library(igraph)
 library(openxlsx2)
 library(httr)
+library(rvest)
+library(XML)
 
 
 # Connexion ----
@@ -54,7 +56,15 @@ data_comm$date_com <- as.Date.character(data_comm$date_com)
 #### Etape 0 : Transformer le type de données pour plus de facilité/performance dans le traitement ----
 URL_var <- as_tibble(data_comm) %>% # Etape 0
   #### Etape 1 : Se limiter aux commentaires avec au moins un lien hypertexte  ----
-  subset(., comm %like% c("%https%","%http%","%www%","%WWW%"))
+  subset(., comm %like% c("%http%","%www%","%WWW%","%HTTP%"))
+
+# Fonction pour supprimer les balises HTML
+remove_html_tags <- function(x) {
+  gsub("[>\\<]", " ", x) # .*? correspond à n'importe quel caractère répété 0 ou plusieurs fois, de manière non gourmande
+}
+
+# Application de la fonction à la colonne "comm"
+URL_var$comm_sans_html <- sapply(URL_var$comm, remove_html_tags)
 
 # Créer une nouvelle colonne pour stocker les URL extraites
 URL_var$urls <- NA
@@ -62,32 +72,46 @@ URL_var$urls <- NA
 # Parcourir chaque ligne et extraire l'URL
 for (i in 1:nrow(URL_var)) {
   # Extraire l'URL en utilisant une expression régulière
-  extracted_url <- str_extract_all(URL_var$comm[i], "(?i)\\b(?:https?://|www\\.)\\S+(?:/|\\b)")  
+  extracted_url <- str_extract_all(URL_var$comm_sans_html[i], "(?i)\\b(?:https?://|www\\.)\\S+(?:/|\\b)")  
   # Stocker l'URL extraite dans la nouvelle colonne
   URL_var$urls[i] <- extracted_url
 }
 
+# "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))"
+
 # séparer les URLs en autant de lignes
 df_split <- unnest(URL_var, urls)
-urls_parses <- url_parse(df_split$urls)
+urls_parses <- url_parse(df_split$urls) 
 df <- merge(df_split, urls_parses, by = "row.names", all = F)
 
-write_xlsx(df, "urls_commentaires.xlsx")
-write_csv(df, "urls_comm.csv")
+# write_xlsx(df, "urls_commentaires.xlsx")
+# write_csv(df, "urls_comm.csv")
+
+# faire un select distinct
+
+urls_v1 <- data.frame(df$Row.names,df$publication,df$inner_id,df$date_com,df[,7:14])
+names(urls_v1)[1:4] <- c("rowname","publication","inner_id","date_com")
+
+a <- subset(urls_v1, urls_v1$server=="")
+# Extraire le protocole (http://)
+protocole <- gsub("^(.*://).*", "\\1", a$urls)
+
+# Extraire le nom de domaine (www.example.com)
+domaine <- gsub("^.*://([^/]+).*", "\\1", a$urls)
+
+# Extraire le chemin (/path/to/)
+chemin <- gsub("^.*://[^/]+(/.*/).*", "\\1", a$urls)
+
+# Extraire le nom de fichier (file.html)
+nom_fichier <- gsub("^.*://[^/]+/.*/(.*)$", "\\1", a$urls)
+
+b <- data.frame(protocole,domaine,chemin)
 
 
-##
-df_split$urls2 <- NA
+urls_vf <- urls_v1[,-1] %>%
+  unique()
 
-for (i in 1:nrow(df_split)) {
-  # Extraire l'URL en utilisant une expression régulière : c'est pour les cas où la chaine de caractères contient un balisage HTML avec des caractères spéciaux qui ne sont pas pris en compte par l'expression régulière précédente
-  extracted_url <- str_extract_all(df_split$urls[i], "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))")
-  # Stocker l'URL extraite dans la nouvelle colonne
-  df_split$urls2[i] <- extracted_url
-}
-
-# séparer les URLs en autant de lignes
-df_split2 <- unnest(df_split, urls2)
-urls_parses2 <- url_parse(df_split2$urls2)
-df_final <- merge(df_split2, urls_parses2, by = "row.names", all = F)
+f2 <- factor(urls_vf$server) |>
+  fct_infreq() |> 
+  questionr::freq()
 
