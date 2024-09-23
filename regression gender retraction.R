007acf971a39905db62e230808fc5efb75f65463
Abdelghani Maddi <maddiabdelghani@gmail.com>
2024-09-23 14:56:30 +0200

    commit - regression

--- regression gender retraction.R
+++ regression gender retraction.R
@@ -64,7 +64,7 @@ bdd_disc_reg_ss_notices2[, 30:33] <- lapply(bdd_disc_reg_ss_notices2[, 30:33], f
 ## Réordonnancement de bdd_disc_reg$Gtype2_rec pour la ref dans la regression
 bdd_disc_reg_ss_notices2$Gtype2_rec <- bdd_disc_reg_ss_notices2$Gtype2_rec %>%
   fct_relevel(
-    "Man alone", "Women only" , "Men-Women | M first", "Men-Women | W first",
+    "Man alone", "Men-Women | M first", "Women only" , "Men-Women | W first",
     "Men only", "Woman alone" 
   )
 
@@ -73,16 +73,23 @@ var_label(bdd_disc_reg_ss_notices2$Gtype2_rec) <- "Gender type"
 var_label(bdd_disc_reg_ss_notices2$is_oa) <- "Is Open Access"
 var_label(bdd_disc_reg_ss_notices2$cited_by_count) <- "Log(1 + Citation count)"
 var_label(bdd_disc_reg_ss_notices2$grants) <- "Grants"
-var_label(bdd_disc_reg_ss_notices2$nb_aut) <- "# Authors"
+var_label(bdd_disc_reg_ss_notices2$nb_aut) <- "Log(#Authors)"
+var_label(bdd_disc_reg_ss_notices2$publication_year) <- "Publication year"
 
 
 # Transform nb_aut into a categorical variable
 bdd_disc_reg_ss_notices2$nb_aut_cat <- cut(bdd_disc_reg_ss_notices2$nb_aut,
-                                           breaks = c(0, 1, 5, 10, 200),  # Intervals
-                                           labels = c("Alone (1)", "Small team (2-5)", "Medium team (6-10)", "Large team (>10)"),
+                                           breaks = c(0, 5, 10, 20, 200),  # Intervals
+                                           labels = c("Small team (1-5)", "Medium team (6-10)", 
+                                                      "Medium-Large team (11-20)", "Large team (>20)"),
                                            right = TRUE)  # Includes upper bound
 ###
-res <- glm(is_retracted ~ Gtype2_rec + is_oa + log(1+cited_by_count) + nb_aut_cat +
+
+bdd_disc_reg_ss_notices2$is_large_team <- ifelse(bdd_disc_reg_ss_notices2$nb_aut >10, 1,0)
+
+###
+res <- glm(is_retracted ~ Gtype2_rec + is_oa + log(1+cited_by_count) + log(nb_aut) +# nb_aut_cat +
+             # is_large_team +
              publication_year + `Health Sciences` + `Life Sciences` + `Physical Sciences`,
            data = bdd_disc_reg_ss_notices2,
            family =  binomial(logit))
